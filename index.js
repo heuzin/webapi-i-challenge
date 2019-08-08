bcd80773099447c452932720b41f400bc1241d85
heuzin <matheusqs.arq@gmail.com>
2019-08-07 20:18:39 -0400

    POST and GET

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db')
+
+const server = express();
+
+server.use(express.json());
+
+
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+
+    db.insert(newUser)
+        .then((name, bio) => {
+            if (name && bio) {
+                res.status(201).json(name);
+            } else {
+                res.status(404).json({
+                    message: "Please provide name and bio for the user."
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                    err: err,
+                    message: "There was an error while saving the user to the database"
+            });
+        });
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.status(500).json({
+                err: err,
+                message: "The users information could not be retrieved."
+            });
+        });
+})
+
+server.listen(4000, () => {
+    console.log('Server is running on port 4000...')
+});
