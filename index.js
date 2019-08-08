59254129fe63b240f3f1a36a87b4aeacea87480c
heuzin <matheusqs.arq@gmail.com>
2019-08-07 20:22:25 -0400

    GET by ID

--- index.js
+++ index.js
@@ -42,6 +42,27 @@ server.get('/api/users', (req, res) => {
         });
 })
 
+server.get("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+
+    db.findById(id)
+        .then(user => {
+            if (user) {
+                res.json(user)
+            } else {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist."
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            err: err,
+            message: "The user information could not be retrieved."
+        });
+    });  
+})
+
 server.listen(4000, () => {
     console.log('Server is running on port 4000...')
 });
